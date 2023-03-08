0bf2591f93abbd488f1d6d3b367c104a2eaa7450
lochan07 <lochan@binaryveda.com>
2023-03-08 21:12:10 +0530

    fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = () => {
     <div className='navbar'>
       <img className='logo' src={Logo} alt='logo'></img>
       <nav className='nav-links'>
-        <a className='link' href=''>Home</a>
-        <a className='link' href=''>Services</a>
-        <a className='link' href=''>Plating Methods</a>
-        <a className='link' href=''>About Us</a>
-        <a className='link' href=''>Contact Us</a>
+        <a className='link' href='/'>Home</a>
+        <a className='link' href='/'>Services</a>
+        <a className='link' href='/'>Plating Methods</a>
+        <a className='link' href='/'>About Us</a>
+        <a className='link' href='/'>Contact Us</a>
       </nav>
       <div className='contact-num'><a href="tel:7715089653">7715089653</a>/<a href="tel:77715090583">7715090583</a></div>
     </div>
